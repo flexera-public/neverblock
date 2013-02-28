1ab6c0f581e697fa8e5998589835b519e4d2cf93
Dominic <dominic@rightscale.com>
2013-02-28 01:51:56 -0800

    acu78070 - added failing spec to show problem with EM.many_ticks not cleaning up its state between runs.
    If a tick is scheduled with EM.many_ticks and EM is stopped and started again. EM.many_ticks' state is not cleaned up and when EM is restarted. The scheduled EM.many_ticks tick wont ever be run because EM has been stopped. Any new ticks scheduled with EM.many_ticks wont ever run because EM.many_ticks thinks that a tick is still pending.

---
+++ spec/em_many_ticks_spec.rb
@@ -0,0 +1,28 @@
+require_relative "spec_helper"
+
+describe "EM.many_ticks" do
+  context "with a stopped EM runs and a scheduled many_ticks tick" do
+    it "it should remove any outstanding ticks after EM has been stopped" do
+      EM.run {
+        # stop gets scheduled for the next tick.
+        EM.stop
+        # Starting a many_ticks tick in the next_tick as well which will get scheduled
+        # in the next_tick after the 'EM.stop' and the tick_queue_running will get marked as running.
+        # The scheduled many_ticks tick will however never happen because EM will get
+        # stopped before.
+        EM.many_ticks{ puts "I am never going to happen"}
+      }
+
+      # After EM stopped it is important to clean up many_ticks. Otherwise scheduled
+      # many_ticks will leave EM.many_ticks in a bad state where @tick_queue_running == true
+      # but since EM has been stopped the scheduled many_ticks tick will never happen and
+      # wont set @tick_queue_running back to true.
+      # 
+      # If EM.many_ticks @tick_queue_running is not back to false, any scheduled
+      # EM.many_ticks ticks wont ever get scheduled.
+      EM.instance_variable_get(:@tick_queue).should be_empty
+      EM.instance_variable_get(:@tick_queue_running).should == false
+    end
+  end
+end
+
