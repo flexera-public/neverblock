3a7d26e676ea2bc18840bee63554057ada6b16e0
Dominic <dominic@rightscale.com>
2013-06-21 08:52:24 -0700

    acu85369 - handling timeout_class == nil
    in 1.9.2, ruby passes nil as the timeout class if
    no timeout was passed  (ex: /usr/lib/ruby/1.9.1/timeout.rb:89:in `timeout' )
    As a result, we are defaulting to to Timeout::Error
    so that we can timeout the fiber by passing the
    exception.

--- lib/neverblock/core/nb_timeout.rb
+++ lib/neverblock/core/nb_timeout.rb
@@ -3,7 +3,7 @@ module NeverBlock
     attr_accessor :time, :error_class
     def initialize(time, error_class)
       @time        = time
-      @error_class = error_class
+      @error_class = error_class || Timeout::Error
     end
   end
 end
